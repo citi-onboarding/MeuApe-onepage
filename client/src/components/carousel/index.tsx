d54b9ac52324edee1c88d4fb63a13ca98a74e241
Maria Clara Albuquerque Moura <mcam3@cin.ufpe.br>
2023-06-09 12:29:08 -0300

    feature/clientCarousel: finished carousel (working properly)

--- client/src/components/carousel/index.tsx
+++ client/src/components/carousel/index.tsx
@@ -10,32 +10,40 @@ const images = [
 export const Carousel: React.FC = () => {
   const [image, setImage] = useState(0);
 
+  const changePhoto = (event: React.MouseEvent<HTMLImageElement>) => {
+    const { width, left } = (event.target as HTMLImageElement).getBoundingClientRect();
+    const position = event.clientX - left;
+    const half = width / 2;
+
+    if (position < half) {
+      if (image === 2) {
+        setImage(1);
+      } 
+      else if (image === 1) {
+        setImage(0);
+      } 
+      else if (image === 0) {
+        setImage(0);
+      }
+    } else {
+      if (image === 2) {
+        setImage(2);
+      } 
+      else if (image === 1) {
+        setImage(2);
+      } 
+      else if (image === 0) {
+        setImage(1);
+      }
+    }
+  };
+
   return (
     <CarouselContainer>
       <CarouselImage
         src={images[image]}
         alt=""
-        onClick={() => {
-          if (image === 2) {
-            if (((image + 1) % images.length) < 3) {
-              setImage(1);
-            } else {
-              setImage(2);
-            }
-          } else if (image === 1) {
-            if (((image + 1) % images.length) < 2) {
-              setImage(0);
-            } else {
-              setImage(2);
-            }
-          } else if (image === 0) {
-            if (((image + 1) % images.length) < 1) {
-              setImage(0);
-            } else {
-              setImage(1);
-            }
-          }
-        }}
+        onClick={changePhoto}
       />
     </CarouselContainer>
   );
