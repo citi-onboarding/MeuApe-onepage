12fd92d360b0bf0d03ae69eb3fadb2a04ea205c7
Maria Clara Albuquerque Moura <mcam3@cin.ufpe.br>
2023-06-14 18:15:30 -0300

    adding link to contact button

--- client/src/pages/contact/index.tsx
+++ client/src/pages/contact/index.tsx
@@ -8,7 +8,7 @@ export const Contact: React.FC = () => {
             <Title>Contato</Title>
             <ContentContainer>
                 <Content>Se interessou? <br /> Vamos conversar!</Content>
-                <Link href="https://wa.me/5581995194672" target="_blank"><ButtonContact><img src={LogoWpp} alt="" /><Ajuste>Entre em contato</Ajuste></ButtonContact></Link>
+                <Link href="https://wa.me//5581991141599" target="_blank"><ButtonContact><img src={LogoWpp} alt="" /><Ajuste>Entre em contato</Ajuste></ButtonContact></Link>
             </ContentContainer>
         </ContactContainer>
     );
